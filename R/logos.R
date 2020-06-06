f9d50979b404033d8acc47845fb0df0f8f1ab834
dennisbrookner <debrookner@gmail.com>
2020-06-06 16:00:47 -0400

    update logos.R to work in package form, with ggimage and nflfastR as dependencies

--- R/logos.R
+++ R/logos.R
@@ -1,8 +1,9 @@
-install.packages("ggimage")
-library(ggimage)
-logos <- teams_colors_logos %>% select(team_abbr, team_logo_espn)
-punts <- punts %>% left_join(logos, by = c("posteam" = "team_abbr"))
+# install.packages("ggimage")
+# library(ggimage)
+
+logos <- nflfastR::teams_colors_logos %>% select(team_abbr, team_logo_espn)
+punts <- punts %>% dplyr::left_join(logos, by = c("posteam" = "team_abbr"))
 for(i in 1:length(miniY$Punter)) {
-  miniY$team_code[i]<-punts$posteam[punts$punter_player_name==miniY$Punter[i]&punts$Year==miniY$year[i]]
+  miniY$team_code[i] <- punts$posteam[punts$punter_player_name==miniY$Punter[i] & punts$Year==miniY$year[i]]
 }
-miniY <- miniY %>% left_join(logos, by = c("team_code" = "team_abbr"))
+miniY <- miniY %>% dplyr::left_join(logos, by = c("team_code" = "team_abbr"))
