af76ba77d71c5347a06f2ccea6b6e17a14d6e671
dennisbrookner <debrookner@gmail.com>
2020-06-06 15:24:17 -0400

    added addition plot as demonstration

--- R/plots.R
+++ R/plots.R
@@ -50,30 +50,45 @@ plot_template <- function(data, x, y,
   }
   if(!is.null(annotation_df)) {
     for(i in 1:length(annotation_df$x)) {
-      plot <- plot + annotate("text", x=annotation_df$x[i], y=annotation_df$y[i], label=annotation_df$label[i])
+      plot <- plot + ggplot2::annotate("text", x=annotation_df$x[i], y=annotation_df$y[i], label=annotation_df$label[i])
     }
   }
 }
 
+career_plot <- function(name, miniY, stat) {
+  data <- miniY %>% dplyr::filter(Punter==name)
+
+  plot_template(data=data, x=year, y=stat,
+                label=year, repel_size=1, repel_padding=1,
+                point_size=NULL, point_alpha=NULL,
+                image=url, image_size=0.05,
+                yintercept=100, xintercept=100, intercept=NULL, slope=NULL,
+                title = glue::glue('Career ', stat, 'for ', name),
+                subtitle = NULL, xlabel = 'Year', ylabel = glue::glue(stat),
+                caption = 'data from nflfastR | figure by @thePuntRunts',
+                annotation_df = NULL)
+
+}
 
 
-  ggplot(data=punts_meta, aes(x=temperature, y=SHARP_RERUN, label=punter_player_name))+
-  #geom_label_repel(size=2, point.padding = 0.1)+
-  geom_point(size=1, alpha=0.2)+
-  #geom_image(aes(image = url), size = 0.04)+
-  geom_hline(yintercept=100, linetype="dotted")+
-  geom_vline(xintercept=mean(mini$temperature, na.rm = TRUE), linetype="dotted")+
-  geom_abline(intercept = 90.988, slope = 0.144374)
-labs(title = "Weathering the weather",
-     #subtitle = "Do punters bring their best to the Super Bowl?",
-     x="Average temperature for a punt attempt", y="SHARP RERUN", caption="figure @ThePuntRunts | data @nflfastR")+
-  # annotate("text", x=97, y=113, label="Precision Punters")+
-  #annotate("text", x=103, y=110, label="Big-stage Punters")+
-  # annotate("text", x=97, y=95, label="Replacement Punters")+
-  #annotate("text", x=103, y=95, label="Career Performers")+
-  theme_bw() +
-  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+#   ggplot(data=punts_meta, aes(x=temperature, y=SHARP_RERUN, label=punter_player_name))+
+#   #geom_label_repel(size=2, point.padding = 0.1)+
+#   geom_point(size=1, alpha=0.2)+
+#   #geom_image(aes(image = url), size = 0.04)+
+#   geom_hline(yintercept=100, linetype="dotted")+
+#   geom_vline(xintercept=mean(mini$temperature, na.rm = TRUE), linetype="dotted")+
+#   geom_abline(intercept = 90.988, slope = 0.144374)
+# labs(title = "Weathering the weather",
+#      #subtitle = "Do punters bring their best to the Super Bowl?",
+#      x="Average temperature for a punt attempt", y="SHARP RERUN", caption="figure @ThePuntRunts | data @nflfastR")+
+#   # annotate("text", x=97, y=113, label="Precision Punters")+
+#   #annotate("text", x=103, y=110, label="Big-stage Punters")+
+#   # annotate("text", x=97, y=95, label="Replacement Punters")+
+#   #annotate("text", x=103, y=95, label="Career Performers")+
+#   theme_bw() +
+#   theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 
 
