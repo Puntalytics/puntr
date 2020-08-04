6faab4e837fe3cd65593dc3a48ba7c7cace34dd2
dennisbrookner <debrookner@gmail.com>
2020-08-04 12:58:28 -0400

    logos now grabs colors as well

--- R/logos.R
+++ R/logos.R
@@ -4,7 +4,7 @@
 add_logos_miniY <- function(miniY, punts) {
 
   if(!"team_logo_espn" %in% colnames(miniY)) {
-    logos <- nflfastR::teams_colors_logos %>% select(team_abbr, team_logo_espn)
+    logos <- nflfastR::teams_colors_logos %>% select(team_abbr, team_logo_espn, team_color, team_color2)
     punts_temp <- punts %>% dplyr::left_join(logos, by = c("posteam" = "team_abbr"))
 
     for(i in 1:length(miniY$punter_player_name)) {
@@ -18,7 +18,7 @@ add_logos_miniY <- function(miniY, punts) {
 
 add_logos <- function(punts) {
   if(!"team_logo_espn" %in% colnames(punts)) {
-    logos <- nflfastR::teams_colors_logos %>% select(team_abbr, team_logo_espn)
+    logos <- nflfastR::teams_colors_logos %>% select(team_abbr, team_logo_espn, team_color, team_color2)
     punts <- punts %>% dplyr::left_join(logos, by = c("posteam" = "team_abbr"))
   }
   return(punts)
