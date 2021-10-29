fc09a7a4ddd94a89154bbc3b645c5c084e2ddfa6
Kazink36 <13jaredlee@gmail.com>
2021-10-29 15:25:34 -0700

    migrate college punts to cfbfastR

--- R/college.R
+++ R/college.R
@@ -1,22 +1,19 @@
 #' Import college punting data
-#' @description Import college punting data for seasons in the scope of \code{cfbscrapR}.
-#' The default \code{weeks} parameter will work for completed seasons; ask for fewer weeks to scrape an in-progress season.
-#' Scraping both completed and in-progress seasons will require two separate function calls (see examples).
-#' This function is a wrapper around \code{cfbscrapR::cfb_pbp_data}.
+#' @description Import college punting data for seasons in the scope of \code{cfbfastR} (back to 2014).
+#' This function is a wrapper around \code{cfbfastR::load_cfb_pbp}.
 #' @param years A year or range of years to be scraped
-#' @param weeks A week or range or weeks, defaults to \code{1:15}. Use fewer weeks to scrape an in-progress season
-#' @return A tibble \code{punts} of punts in the \code{cfbscrapR} format
+#' @return A tibble \code{punts} of punts in the \code{cfbfastR} format
 #' @examples
 #' \dontrun{
-#' import_college_punts(2018:2019) # completed seasons
-#' import_college_punts(2020, weeks = 1) # just week 1 of the 2020 season
+#' import_college_punts(2018:2019)
 #' }
 #' @export
-import_college_punts <- function(years, weeks=1:15) {
-  punts <- tidyr::tibble(week = weeks) %>% tidyr::expand(week, year=years) %>%
-    dplyr::mutate(pbp = purrr::map2(year, week, cfbscrapR::cfb_pbp_data, season_type='both', play_type="punt")) %>%
-    dplyr::select(-c(week,year)) %>%
-    tidyr::unnest(cols = pbp)
+import_college_punts <- function(years) {
+  punts <- map_df(years, function(x){
+    cfbfastR::load_cfb_pbp(x) %>%
+      dplyr::filter(punt == 1) %>%
+      dplyr::mutate(season = x)
+  })
   return(punts)
 }
 
@@ -24,7 +21,7 @@ import_college_punts <- function(years, weeks=1:15) {
 #'
 #' @description Rename columns and process data such that the output can be plugged directly into \code{puntr::calculate_all},
 #' and the output of that can be plugged directly into \code{puntr::create_mini} (or \code{puntr::create_miniY}).
-#' @param punts A data frame containing punts in the cfbscrapR format
+#' @param punts A data frame containing punts in the cfbfastR format
 #' @param power_five Logical, defaults to TRUE to include only punters from Power 5 teams
 #' @return A tibble \code{punts} in a format usable for \code{puntr::calculate_all}
 #' @examples
@@ -35,27 +32,37 @@ import_college_punts <- function(years, weeks=1:15) {
 college_to_pro <- function(punts, power_five = TRUE) {
 
   punts <- punts %>%
-    dplyr::rename(season = year) %>%
+    #dplyr::rename(season = year) %>%
     dplyr::mutate(GrossYards = play_text %>%
              stringr::str_extract("punt for [:digit:]+") %>%
              stringr::str_extract("[:digit:]+") %>%
              as.numeric()) %>%
-    dplyr::filter(!is.na(GrossYards)) %>%
-    dplyr::mutate(return_yards = as.numeric(yards_gained)) %>%
-    dplyr::filter(!is.na(return_yards)) %>%
-    dplyr::mutate(NetYards = GrossYards - return_yards) %>%
+    #dplyr::filter(!is.na(GrossYards)) %>%
+    dplyr::mutate(return_yards = as.numeric(yds_punt_return),
+                  return_yards = ifelse(is.na(return_yards),0,return_yards),
+                  return_yards = ifelse(str_detect(play_text,"loss"),-1*return_yards,return_yards)) %>%
+    #dplyr::filter(!is.na(return_yards)) %>%
     dplyr::mutate(punter_player_name = play_text %>%
              stringr::str_extract(".+(?= punt for)")) %>%
     dplyr::mutate(YardsFromOwnEndZone = as.integer(100 - yards_to_goal)) %>%
     dplyr::filter(YardsFromOwnEndZone <= 70) %>%
     dplyr::mutate(touchback = play_text %>% stringr::str_detect("ouchback")) %>%
+    dplyr::mutate(return_yards = dplyr::if_else(touchback, 0, return_yards)) %>%
+    dplyr::mutate(NetYards = GrossYards - return_yards) %>%
     dplyr::mutate(GrossYards = dplyr::if_else(touchback, as.numeric(GrossYards-20), as.numeric(GrossYards))) %>%
     dplyr::mutate(punt_out_of_bounds = play_text %>% stringr::str_detect("out.of.bounds")) %>%
     dplyr::mutate(punt_fair_catch = play_text %>% stringr::str_detect("air catch")) %>%
     dplyr::mutate(punt_downed = play_text %>% stringr::str_detect("downed")) %>%
-    dplyr::mutate(PD = dplyr::if_else(YardsFromOwnEndZone >=41, 1, 0))
+    dplyr::mutate(PD = dplyr::if_else(YardsFromOwnEndZone >=41, 1, 0)) %>%
+    #rename columns to avoid breaking calculate_all()
+    dplyr::rename(ep_before_cfb = ep_before,
+                  ep_after_cfb = ep_after)
 
-  team_info <- cfbscrapR::cfb_team_info()
+
+
+  # Pull from data-repo to avoid requiring cfbd API key
+  #team_info <- cfbfastR::cfbd_team_info()
+  team_info <- readRDS(url("https://github.com/saiemgilani/cfbfastR-data/blob/master/team_info/rds/cfb_team_info_2020.rds?raw=true"))
 
   if(power_five) {
     team_info <- team_info %>%
